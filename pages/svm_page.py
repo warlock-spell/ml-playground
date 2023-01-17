f6d1a6c013f750004e0a53d7bfaac243b5c6fb2a
warlock-spell <83418141+warlock-spell@users.noreply.github.com>
2023-01-17 20:08:46 +0530

    create dataset ui added

--- pages/svm_page.py
+++ pages/svm_page.py
@@ -18,12 +18,58 @@ dash.register_page(__name__, title="SVM", name="SVM", order=page_order["svm_page
 learn = dbc.Container("Hello")
 
 # Select sample size
+select_sample_size = html.Div(
+    [
+        dbc.Label("Choose number of features"),
+        dbc.RadioItems(
+            options=[
+                {"label": "Sample size: 50", "value": 50},
+                {"label": "Sample size: 75", "value": 75},
+                {"label": "Sample size: 100", "value": 100},
+                {"label": "Sample size: 150", "value": 150},
+            ],
+            value=50,
+            id="svm-select-sample-size",
+            inline=True,
+        ),
+    ]
+)
 
 # Select Features
+select_features = html.Div(
+    [
+        dbc.Label("Choose number of features"),
+        dbc.RadioItems(
+            options=[
+                {"label": "No of features: 2", "value": 2},
+                {"label": "No of features: 3 (coming soon!)", "value": 3, "disabled": True},
+            ],
+            value=2,
+            id="svm-select-no-of-features",
+            inline=True,
+        ),
+    ]
+)
 
 # Select Mean
+select_mean = html.Div(
+    [
+        dbc.Label("Choose Mean of the Dataset(To be generated)"),
+        dcc.Slider(-5, 5, 1,
+                   value=1,
+                   id='svm-select-mean-slider'
+                   ),
+    ])
 
 # Select SD
+select_sd = html.Div(
+    [
+        dbc.Label("Choose Standard deviation of the Dataset(To be generated)"),
+        dcc.Slider(1, 3, 0.2,
+                   value=1,
+                   id='svm-select-mean-slider'
+                   ),
+    ])
 
 X, y = datasets.make_blobs(n_samples=50, n_features=2, centers=2, cluster_std=1.05, random_state=40)
 y = np.where(y == 0, -1, 1)
@@ -63,6 +109,11 @@ def get_coordinates():
 
 all_coordinates = get_coordinates()
 
-play = dbc.Container("Hi")
+play = dbc.Container(html.Div([
+    select_sample_size,
+    select_features,
+    select_mean,
+    select_sd
+]))
 
 layout = create_page_layout(learn_tab=learn, play_tab=play)
