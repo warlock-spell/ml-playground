8f7643b1331da5b080fec5257db0763b99f48982
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-23 22:03:46 +0530

    interactive visualiztions

--- pages/linear_regression_page.py
+++ pages/linear_regression_page.py
@@ -10,45 +10,182 @@ from src.component.page_layout import create_page_layout
 import dash_bootstrap_components as dbc
 from src.component.page_order import page_order
 from content.linear_regression import content
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from plotly.subplots import make_subplots
+from src.component import ids
+import plotly.graph_objects as go
+import numpy as np
+from algorithm.linear_regression import LinearRegression
+from itertools import chain
 
 dash.register_page(__name__, title="Linear Regression", name="Linear Regression",
                    order=page_order["linear_regression_page"])
-df = px.data.tips()
+
 learn = dbc.Container([dcc.Markdown(content, dangerously_allow_html=True, mathjax=True)])
-play = dbc.Container(html.Div(
+
+# select sample size
+select_sample_size = html.Div([
+    dbc.RadioItems(id=ids.LR_SELECT_SAMPLE_SIZE,
+                   options=[
+                       {'Size': '50', 'value': 50},
+                       {'Size': '100', 'value': 100},
+                       {'Size': '150', 'value': 150},
+                       {'Size': '200', 'value': 200},
+                   ],
+                   value=100, inline=True)])
+
+NUMBER_OF_FEATURES = 1
+
+# select noise
+select_noise = html.Div([
+    dbc.RadioItems(id=ids.LR_SELECT_NOISE,
+                   options=[
+                       {'Noise': '10', 'value': 10},
+                       {'Noise': '20', 'value': 20},
+                       {'Noise': '30', 'value': 30},
+                   ],
+                   value=20, inline=True)])
+
+# select random state
+select_random_state = html.Div(
+    [dbc.RadioItems(id=ids.LR_SELECT_RANDOM_STATE,
+                    options=[
+                        {'Random State': '1', 'value': 1},
+                        {'Random State': '2', 'value': 2},
+                        {'Random State': '3', 'value': 3},
+                        {'Random State': '4', 'value': 4},
+                    ],
+                    value=4, inline=True)])
+
+# select test size
+select_test_size = html.Div(
     [
-        dbc.Row([
-            dbc.Col(
-                [
-                    html.Img(src='assets/smoking2.jpg')
-                ], width=4
-            ),
-            dbc.Col(
-                [
-                    dcc.RadioItems(df.day.unique(), id='day-choice', value='Sat')
-                ], width=6
-            )
-        ]),
-        dbc.Row([
-            dbc.Col(
-                [
-                    dcc.Graph(id='bar-fig',
-                              figure=px.bar(df, x='smoker', y='total_bill'))
-                ], width=12
-            )
-        ])
+        dbc.Label("Choose test size (in fraction)"),
+        dcc.Slider(0.05, 0.3, 0.05,
+                   value=0.2,
+                   id=ids.LR_SELECT_TEST_SIZE,
+                   ),
+    ])
+
+# select learning rate - alpha
+select_alpha = html.Div(
+    [dbc.RadioItems(id=ids.LR_SELECT_ALPHA,
+                    options=[
+                        {'Learning Rate': '0.0001', 'value': 0.0001},
+                        {'Learning Rate': '0.001', 'value': 0.001},
+                        {'Learning Rate': '0.005', 'value': 0.005},
+                        {'Learning Rate': '0.01', 'value': 0.01},
+                        {'Learning Rate': '0.05', 'value': 0.05},
+                    ],
+                    value=0.001, inline=True)])
+
+# graph component
+# figure
+lr_graph = html.Div([
+    dbc.Label("Let's see the results"),
+    dcc.Graph(figure={}, id=ids.LR_GRAPH_VIZ),
+])
+
+# error component
+show_error = html.Div(
+    [
+        html.Br(),
+        html.P(id=ids.LR_SHOW_ERROR),
     ]
-))
+)
+
+# dataset creation
+def create_dataset(n_samples, n_features, noise, random_state, test_size):
+    X, y = datasets.make_regression(n_samples=n_samples, n_features=n_features, noise=noise, random_state=random_state)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=1234
+    )
+    return X, y, X_train, X_test, y_train, y_test
+
+
+# error function
+def mse(y_true, y_predicted):
+    return np.mean((y_true - y_predicted) ** 2)
+
 
 
 @callback(
-    Output('bar-fig', 'figure'),
-    Input('day-choice', 'value')
+    Output(ids.LR_GRAPH_VIZ, "figure"),
+    Input(ids.LR_SELECT_SAMPLE_SIZE, "value"),
+    Input(ids.LR_SELECT_NOISE, "value"),
+    Input(ids.LR_SELECT_RANDOM_STATE, "value"),
+    Input(ids.LR_SELECT_TEST_SIZE, "value"),
+    Input(ids.LR_SELECT_ALPHA, "value"),
 )
-def update_graph(value):
-    dff = df[df.day == value]
-    fig = px.bar(dff, x='smoker', y='total_bill')
+def create_dataset_and_plot(sample_size, noise, random_state, test_size, alpha):
+    X, y, X_train, X_test, y_train, y_test = create_dataset(n_samples=sample_size,
+                                                            n_features=NUMBER_OF_FEATURES,
+                                                            noise=noise,
+                                                            random_state=random_state,
+                                                            test_size=test_size)
+
+    # print(X, y)
+
+    # initializing regressor
+    regressor = LinearRegression(lr=alpha)
+    regressor.fit(X_train, y_train)
+    predicted_values = regressor.predict(X_test)
+
+    # plotting logic
+    X_test = list(chain(*X_test))
+    X = list(chain(*X))
+    # Define data for scatter plot
+    scatter_data = go.Scatter(x=X, y=y, mode='markers')
+    # Define data for line plot
+    line_data = go.Scatter(x=X_test, y=predicted_values, mode='lines')
+    # Define layout for the graph
+    layout = go.Layout(title='Scatter Plot with Line', xaxis=dict(title='X-Axis'), yaxis=dict(title='Y-Axis'))
+    # Combine scatter and line data in one figure
+    fig = go.Figure(data=[scatter_data, line_data], layout=layout)
+
     return fig
 
+
+# error component
+@callback(
+    Output(ids.LR_SHOW_ERROR, "children"),
+    Input(ids.LR_SELECT_SAMPLE_SIZE, "value"),
+    Input(ids.LR_SELECT_NOISE, "value"),
+    Input(ids.LR_SELECT_RANDOM_STATE, "value"),
+    Input(ids.LR_SELECT_TEST_SIZE, "value"),
+    Input(ids.LR_SELECT_ALPHA, "value"),
+)
+def create_dataset_and_plot(sample_size, noise, random_state, test_size, alpha):
+    X, y, X_train, X_test, y_train, y_test = create_dataset(n_samples=sample_size,
+                                                            n_features=NUMBER_OF_FEATURES,
+                                                            noise=noise,
+                                                            random_state=random_state,
+                                                            test_size=test_size)
+    regressor = LinearRegression(lr=alpha)
+    regressor.fit(X_train, y_train)
+    predicted_values = regressor.predict(X_test)
+    mse_i_value = mse(y_test, predicted_values)
+    return mse_i_value
+
+
+play = dbc.Container(html.Div(
+    [
+        html.Br(),
+        select_sample_size,
+        html.Br(),
+        select_noise,
+        html.Br(),
+        select_random_state,
+        html.Br(),
+        select_test_size,
+        html.Br(),
+        select_alpha,
+        html.Br(),
+        show_error,
+        html.Br(),
+        lr_graph,
+    ]
+))
 
 layout = create_page_layout(learn_tab=learn, play_tab=play)
--- src/component/ids.py
+++ src/component/ids.py
@@ -4,10 +4,8 @@
 # @Time:        09-12-2022 11:45 pm
 
 # off_canvas component
-ABOUT_ME="about_me"
-OFFCANVAS_SCROLLABLE="offcanvas_scrollable"
-
-
+ABOUT_ME = "about_me"
+OFFCANVAS_SCROLLABLE = "offcanvas_scrollable"
 
 # SVM PAGE
 SVM_SELECT_SAMPLE_SIZE = "svm_select_sample_size"
@@ -16,3 +14,12 @@ SVM_SELECT_MEAN = "svm_select_mean_slider"
 SVM_SELECT_SD = "svm_select_sd_slider"
 SVM_GRAPH_VIZ = "svm_graph_viz"
 SVM_GRAPH_DATA = "svm_graph_data"
+
+# Linear Regression Page
+LR_SELECT_SAMPLE_SIZE = "select-sample-size-linear-regression"
+LR_SELECT_NOISE = "select-noise-linear-regression"
+LR_SELECT_RANDOM_STATE = "select-random-state-linear-regression"
+LR_SELECT_TEST_SIZE = "select-test-size-linear-regression"
+LR_SELECT_ALPHA = "select-alpha-linear-regression"
+LR_GRAPH_VIZ = "linear-regression-graph"
+LR_SHOW_ERROR = "show-error-linear-regression"
